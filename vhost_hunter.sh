a414162d852e51bddd4da09440b72b00af7ff874
SSHad0w <60116694+SSHAD0w@users.noreply.github.com>
2021-12-30 22:15:35 -0500

    Fixed path
    Somehow I got a oneliner wrong

--- vhost_hunter.sh
+++ vhost_hunter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Enter the url!: "
 read url
-gobuster vhost -u $url -w /usr/share/seclists/Discovery/DNS/subdomains/-topmillion-110000.txt
+gobuster vhost -u $url -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt
